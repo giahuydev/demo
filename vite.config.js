b775d0ed215429f7bd525fe11068d2a2604822e4
giahuydcwizard <huy.trangia@dcwizard.net>
2025-11-23 18:14:25 +0700

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 // vite.config.js
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Khi FE gọi /identity, Vite sẽ chuyển hướng đến http://localhost:8080
+      "/identity": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
